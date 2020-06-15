21beac8e4fc7f0fc4b82029febdf61cd65636ca4
Nishant Baheti <nishantbaheti.it19@gmail.com>
2020-06-15 19:07:26 +0000

    udpate

--- nodeJsApi/index.js
+++ nodeJsApi/index.js
@@ -1,13 +1,21 @@
 var express = require("express");
-var app = express()
-var port = 5000
+var app = express();
+var port = 5000;
 
+app.get("/", function (req, res) {
+  res.status(200).send({
+    status: "success",
+    message: "hello world.",
+  });
+});
 
-app.get('/',function(req,res){
-    res.send("hello world.")
-})
+app.get("/about", function (req, res) {
+  res.status(200).send({
+    status: "success",
+    author: "Nishant Baheti",
+  });
+});
 
-
-app.listen(port,function(){
-    console.log("app listening at port "+port)
-})
+app.listen(port, function () {
+  console.log("app listening at port " + port);
+});
