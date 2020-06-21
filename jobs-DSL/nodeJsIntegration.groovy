d9438b601aa587f7ca013ab8027c23dd71635f7d
Nishant Baheti <nishantbaheti.it19@gmail.com>
2020-06-21 18:30:26 +0530

    Adding Job-DSL forNodeJS pipeline

---
+++ jobs-DSL/nodeJsIntegration.groovy
@@ -0,0 +1,14 @@
+job('NodeJs Example'){
+	scm {
+		git('https://github.com/NishantBaheti/jenkins_exploration.git')
+	}
+	triggers {
+		scm('H/5 * * * *')
+	}
+	wrappers {
+		nodeks('nodejs')
+	}
+	steps {
+		shell("npm install")
+	}
+}
